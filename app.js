3701291206cfda2fbd3ad5a0439e43ff9a90c029
Faizan Ahmed Lakho <faizanahmedlakho001@gmail.com>
2025-11-12 18:44:44 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+function Sub (a,b){
+ return a-b;
+}
+
+
+ function divide (a,b){
+return a/b;
+ }
+
+
+  function add (a,b){
+return a+b;
+ }
+const multiply =(a,b)=> a*b;
+let sum=Sub
+
+console.log(sum(20,9));
+
+console.log(divide(20,9));
+ 
+console.log(add(20,9));
+
+console.log (multiply(20,9));
+
+  const greet = (name) => `hello ${name}!`;
+  console.log (greet('sara'));
+
+
+  
